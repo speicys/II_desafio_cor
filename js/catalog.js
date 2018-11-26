f9bbc1bbeac03b27207af3e41cfeb1aea9fa1233
Roberto Speicys Cardoso <speicys@gmail.com>
2018-11-26 15:31:01 -0200

    Now showing old list of data available. Must be updated for HackAmericas

--- js/catalog.js
+++ js/catalog.js
@@ -3,7 +3,7 @@
 
 var searcher;
 
-d3.csv("/hackemtu-data-catalog/assets/hackemtu-data-catalog.csv", function callback(data){
+d3.csv("/hackamericas/assets/hackamericas.csv", function callback(data){
     // sort data by title
    // data.sort(function(a, b) {
    //     return a.title > b.title;
